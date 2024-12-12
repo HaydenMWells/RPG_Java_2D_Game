1db52a715c3e68e9cb80bbfba5571c9ad12a4429
Hayden Wells <hayden3wells@icloud.com>
2024-12-11 20:01:17 -0500

    Test 1

---
+++ Main/src/main/java/Main/MainApplication.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Test");
+    }
+
+}
